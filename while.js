9b11d8a827b530c09f31b247e9cf79ab24e09796
Makoto Tanabe <xekid78@outlook.com>
2018-03-17 12:31:21 +0900

    Add files via upload

---
+++ while.js
@@ -0,0 +1,14 @@
+console.log("** ケース１ **");
+var i = 1;
+while (i <= 5) {
+    console.log(i + "だよ。");
+    i += 1;
+}
+console.log();
+
+console.log("** ケース２ **");
+var i = 5;
+while (i >= 1) {
+    console.log(i + "だよ。");
+    i -= 1;
+}
